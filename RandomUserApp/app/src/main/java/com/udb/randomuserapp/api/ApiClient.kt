27494ce062974a1ab1e18bd88c725aeb8f942d30
edenilson36 <cr241530@alumno.udb.edu.sv>
2025-09-29 22:40:32 -0600

    Parte 2

---
+++ RandomUserApp/app/src/main/java/com/udb/randomuserapp/api/ApiClient.kt
@@ -0,0 +1,40 @@
+package com.udb.randomuserapp.api
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object ApiClient {
+
+    private const val BASE_URL = "https://randomuser.me/"
+
+    // Configurar OkHttp con logging (para debug)
+    private val okHttpClient: OkHttpClient by lazy {
+        val loggingInterceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .build()
+    }
+
+    // Crear instancia de Retrofit
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    // Crear instancia de la API
+    val randomUserApi: RandomUserApi by lazy {
+        retrofit.create(RandomUserApi::class.java)
+    }
+}
